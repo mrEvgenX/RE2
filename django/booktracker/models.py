8521c08ff7a8a4a60d121e579be091c9cb2eebeb
Evgene <mrEvgenix@gmail.com>
2020-01-24 21:35:43 +0700

    Create models: author, book and shelf

--- django/booktracker/models.py
+++ django/booktracker/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=140)
+    last_name = models.CharField(max_length=140)
+    patronymic = models.CharField(max_length=140, null=True, blank=True)
+
+    def __str__(self):
+        if self.patronymic:
+            return '{} {} {}'.format(self.first_name, self.patronymic, self.last_name)
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=140)
+    author = models.ForeignKey(to=Author, on_delete=models.CASCADE, related_name='books')
+    year = models.IntegerField()
+    publisher = models.CharField(max_length=140, null=True, blank=True)
+    description = models.TextField()
+    shelved_by = models.ManyToManyField(
+        to=settings.AUTH_USER_MODEL,
+        through='ShelvedBook',
+    )
+
+    def __str__(self):
+        return '{} "{}"'.format(self.author, self.title)
+
+
+class ShelvedBook(models.Model):
+    book = models.ForeignKey(to=Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    started = models.DateField()
+    finished = models.DateField()
+
+    class Meta:
+        unique_together = ('book', 'user')
--- django/config/settings.py
+++ django/config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 're2',
+        'USER': 're2',
+        'PASSWORD': 'development',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
