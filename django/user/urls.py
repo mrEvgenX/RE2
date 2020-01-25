2c0b9f16fea485872dcf1903032b115130a820b9
Evgene <mrEvgenix@gmail.com>
2020-01-25 13:29:50 +0700

    Add profile page

--- django/user/urls.py
+++ django/user/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from user.views import RegisterView
+from user.views import RegisterView, ProfileView
 
 
 app_name = 'user'
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('register', RegisterView.as_view(), name='register'),
+    path('profile', ProfileView.as_view(), name='profile'),
 ]
--- django/user/views.py
+++ django/user/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
 from django.urls import reverse
 
 
@@ -9,3 +11,11 @@ class RegisterView(CreateView):
 
     def get_success_url(self):
         return reverse('user:login')
+
+
+class ProfileView(DetailView):
+    model = get_user_model()
+    template_name = 'user/profile.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
