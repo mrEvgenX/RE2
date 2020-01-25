4ac2330917734fa67bb36cfea28555d16d04acdc
Evgene <mrEvgenix@gmail.com>
2020-01-25 12:10:11 +0700

    Add book detail

--- django/booktracker/urls.py
+++ django/booktracker/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
 from django.urls import path
-from booktracker.views import ListAllBooks
+from booktracker.views import ListAllBooks, BookDetail
 
 
 app_name = 'booktracker'
 urlpatterns = [
-    path('', ListAllBooks.as_view(), name='books_list'),
+    path('', ListAllBooks.as_view(), name='all_books'),
+    path('books/<int:pk>', BookDetail.as_view(), name='book_detail'),
 ]
--- django/booktracker/views.py
+++ django/booktracker/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from booktracker.models import Book
 
 
 class ListAllBooks(ListView):
+    model = Book
+
+
+class BookDetail(DetailView):
     model = Book
