fbe5a4a029042d9d195f9bb51f610fbe5f524359
Evgene <mrEvgenix@gmail.com>
2020-01-30 09:33:35 +0700

    Let users post three kinds of messages

--- django/thoughtkeeper/forms.py
+++ django/thoughtkeeper/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from thoughtkeeper.models import IntentionNote
+from thoughtkeeper.models import IntentionNote, MarginNote, Feedback
 
 
 # TODO нужно грамотно оформить, как вроде get_user_model
@@ -24,3 +24,37 @@ class IntentionCommitForm(forms.ModelForm):
     class Meta:
         model = IntentionNote
         fields = ['author', 'entity', 'body', 'is_private']
+
+
+class LeaveMarginNoteForm(forms.ModelForm):
+    author = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=get_user_model().objects.all(),
+        disabled=True,
+    )
+    entity = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=get_entity_model().objects.all(),
+        disabled=True,
+    )
+
+    class Meta:
+        model = MarginNote
+        fields = ['author', 'entity', 'body', 'is_private']
+
+
+class LeaveFeedbackForm(forms.ModelForm):
+    author = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=get_user_model().objects.all(),
+        disabled=True,
+    )
+    entity = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=get_entity_model().objects.all(),
+        disabled=True,
+    )
+
+    class Meta:
+        model = Feedback
+        fields = ['author', 'entity', 'body', 'rating']
--- django/thoughtkeeper/views.py
+++ django/thoughtkeeper/views.py
@@ -2,13 +2,13 @@ from django.urls import reverse
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
-from thoughtkeeper.forms import IntentionCommitForm
-from thoughtkeeper.forms import get_entity_model
+import thoughtkeeper.forms
 
 
-class CommitIntentionView(LoginRequiredMixin, CreateView):
-    form_class = IntentionCommitForm
-    template_name = 'thoughtkeeper/commit_intention.html'
+class PostNoteView(CreateView):
+    template_name = 'thoughtkeeper/post_note.html'
+    page_title = None
+    page_description = None
 
     def get_initial(self):
         return {
@@ -16,8 +16,34 @@ class CommitIntentionView(LoginRequiredMixin, CreateView):
             'author': self.request.user.id,
         }
 
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx.update({
+            'title': self.page_title,
+            'description': self.page_description,
+        })
+        return ctx
+
     def get_success_url(self):
         return reverse(settings.THOUGHTKEEPER_COMMIT_INTENTION_REDIRECT, kwargs={'pk': self.kwargs['pk']})
 
     def get_entity(self):
-        return get_entity_model().objects.get(pk=self.kwargs['pk'])
+        return thoughtkeeper.forms.get_entity_model().objects.get(pk=self.kwargs['pk'])
+
+
+class CommitIntentionView(LoginRequiredMixin, PostNoteView):
+    form_class = thoughtkeeper.forms.IntentionCommitForm
+    page_title = 'Commit intention'
+    page_description = 'Write your expectations about the book.'
+
+
+class LeaveMarginNoteView(LoginRequiredMixin, PostNoteView):
+    form_class = thoughtkeeper.forms.LeaveMarginNoteForm
+    page_title = 'Jot down something'
+    page_description = 'Place here your insights, quotes you liked or anything you found to be usefull.'
+
+
+class LeaveFeedbackView(LoginRequiredMixin, PostNoteView):
+    form_class = thoughtkeeper.forms.LeaveFeedbackForm
+    page_title = 'Leave feedback'
+    page_description = 'What do you think about it?'
